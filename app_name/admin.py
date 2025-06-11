0d864bdfd58c0bd14328d8d1595db7bc927087a2
Hammad <w1971490@westminster.co.uk>
2025-06-11 11:01:04 +0100

    Added admin.py

--- app_name/admin.py
+++ app_name/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from .models import Team, Profile, HealthCard, Session, Vote
 
-# Register your models here.
+@admin.register(Team)
+class TeamAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    search_fields = ('name',)
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'role', 'team')
+    list_filter = ('role', 'team')
+    search_fields = ('user__username',)
+
+
+@admin.register(HealthCard)
+class HealthCardAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title')
+    search_fields = ('title',)
+
+
+@admin.register(Session)
+class SessionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'created_by', 'start_time', 'end_time', 'is_active')
+    list_filter = ('is_active',)
+    search_fields = ('name', 'created_by__username')
+
+
+@admin.register(Vote)
+class VoteAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'session', 'card', 'color')
+    list_filter = ('color', 'session')
+    search_fields = ('user__username', 'card__title', 'session__name')
